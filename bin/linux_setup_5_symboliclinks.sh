c7d89b95b9d3ffedf3bc9fecdf03bc00beb4e352
brentjm <brent_maranzano@yahoo.com>
2018-01-17 15:09:12 -0500

    Working on eslint files

--- bin/linux_setup_5_symboliclinks.sh
+++ bin/linux_setup_5_symboliclinks.sh
@@ -2,7 +2,7 @@
 
 function make_links() {
     cd ~
-    for element in .bashrc .vimrc .inputrc .gvimrc .vim .tern-config .eslintrc .jshintrc bin
+    for element in .bashrc .vimrc .inputrc .gvimrc .vim .tern-config .eslintrc bin
     do
         [ -e "$element" ] && mv "$element" "$element_old"
         ln -s dotfiles/"$element"
