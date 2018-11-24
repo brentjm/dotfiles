ff14f7ffe088c2ca3b1bf28e86c80ad13b95be21
brentjm <brent_maranzano@yahoo.com>
2018-11-23 21:14:18 -0500

    Updated docker installation

--- bin/linux_setup_7_install_extras_as_root.sh
+++ bin/linux_setup_7_install_extras_as_root.sh
@@ -80,20 +80,24 @@ function postgres() {
 }
 
 function install_docker() {
+    # https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-18-04
+    sudo apt-get update
+    sudo apt install apt-transport-https ca-certificates curl software-properties-common
     sudo -u brent curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo apt-key fingerprint 0EBFCD88
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     sudo apt-get update
-    sudo apt-get -y install docker-ce
-    sudo usermod -aG docker brent
+    apt-cache policy docker-ce
+    #sudo apt-get -y install docker-ce
+    #sudo usermod -aG docker brent
 }
 
 #linuxHeaders
 #libncurses
 #googleChrome
-virtualBox
+#virtualBox
 #dvdcss
 #youtubeDL
 #blender
 #postgres
-#install_docker
+install_docker
